0a3ec20d106f4958af999942f91dd5676cbb81f4
karthick R <84372582+Karthick2505@users.noreply.github.com>
2021-06-13 18:06:29 +0530

    Update day2.py

--- day2.py
+++ day2.py
@@ -1,38 +1,35 @@
 #print  value
-
 print("30 days 30 hour challenge")
 
-print('30 days 30 hour challenge')
-
-
 #Assigning String to Variable:
-
 Hours = "thirty"
 print(Hours)
 
 #Indexing using String:
-Days = "Thirty days"
+Days = "karthickRaman"
 print(Days[0])
 
 #print the particular character from certain text?
 Challenge = "I will win"
 print(Challenge[7:10])
+
 #Print the length of Character:
-Challenge = "I will win"
+Challenge = "karthickraman"
 print(len(Challenge))
+
 #Convert String into lower character;
-Challenge = "I will win"
+Challenge = "KARTHICK"
 print(Challenge.lower())
 
 #String Concatenation – Joining two strings
-a = "30 Days"
-b = "30 hours"
+a = "KARTHICK"
+b = "RAMAN"
 c = a + b
 print(c)
 
 #Adding space during concatenation
-a = "30 Days"
-b = "30 hours"
+a = "KARTHICK"
+b = "RAMAN"
 c = a + " " + b
 print(c)
 
