9dc9bbc93ad187b2589157b6f6f1d19e5c2b088b
karthick R <84372582+Karthick2505@users.noreply.github.com>
2021-06-17 13:21:51 +0530

    Update 3day.py

--- 3day.py
+++ 3day.py
@@ -1,4 +1,4 @@
->>> #importing library
+ #importing library
 from tkinter import *
 from tkinter import ttk
 
@@ -6,9 +6,9 @@ window = Tk()
 #Declaring Window Title
 window.title("Registration Form")
 #Declaring Window size
-window.geometry('500x500')
+window.geometry('500x400')
 #Declaring Window Color
-window.configure(background = "violet");
+window.configure(background = "orange");
 #below four fields are declared
 Firstname = Label(window ,text = "First Name",width = 20).grid(row = 0,column = 0)
 LastName = Label(window ,text = "Last Name",width = 20).grid(row = 1,column = 0)
@@ -17,6 +17,7 @@ Mobile = Label(window ,text = "Contact Number",width = 20).grid(row = 3,column =
 SchoolName = Label(window,text = "School Name",width = 20).grid(row = 4,column = 0)
 CollegeName = Label(window,text = "College Name",width = 20).grid(row = 5,column = 0)
 Pincode= Label(window,text = "Pincode",width = 20).grid(row = 6,column = 0)
+selectstate= Label(window,text = "select state",width = 20).grid(row = 7,column = 0)
 
 Firstname1 = Entry(window).grid(row = 0,column = 1)
 Lastname1 = Entry(window).grid(row = 1,column = 1)
@@ -25,11 +26,11 @@ Mobile1 = Entry(window).grid(row = 3,column = 1)
 SchoolName = Entry(window).grid(row = 4,column = 1)
 CollegeName = Entry(window).grid(row = 5,column = 1)
 Pincode = Entry(window).grid(row = 6,column = 1)
-
 StateName = Label(window,text="State",width=20).grid(row = 7,column = 0)
 
+
 #list of states
-list_of_states = ['Arunachal Pradesh','Bihar','Chandigarh','Himachal Pradesh','TamilNadu','Kerala','Karnataka']
+list_of_states = ['Arunachal Pradesh','Bihar','Chandigarh','Himachal Pradesh','TamilNadu','Kerala','Karnataka','Delhi']
 
 #drop down menu
 a = StringVar()
@@ -50,5 +51,5 @@ Radiobutton(window,text = "Female",padx = 25,variable = b,value = 2).grid(row = 
 def clicked():
 	window = "Welcome to " + txt.get()
 label.configure(text= window)
-Button(window ,text="Submit",width = 15).grid(row=18,column=0)
+button=Button(window ,text="Submit",width = 15).grid(row=12,column=2)
 window.mainloop()
